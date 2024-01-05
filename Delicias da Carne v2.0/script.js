7846127efcd32ef4802d3d6d4b1c102fb6211ea4
Dudu <137932110+EduardoFrancisc@users.noreply.github.com>
2024-01-05 12:59:07 -0300

    responsividade no sobre nós

--- Delicias da Carne v2.0/script.js
+++ Delicias da Carne v2.0/script.js
@@ -1,18 +1,20 @@
 let lista = document.getElementById("lista");
-
 let hamburguer = document.getElementById("hamburguer");
 
-hamburguer.addEventListener("click",function () {
-    lista.classList.toggle("active");
-    if (lista.className == "header__ul active") {
-        hamburguer.classList.toggle("fa-xmark")
-    }else{
-        hamburguer.classList.remove("fa-xmark")
-    }
+hamburguer.addEventListener("click", function () {
+  lista.classList.toggle("active");
+  if (lista.className == "header__ul active") {
+    hamburguer.classList.toggle("fa-xmark");
+  } else {
+    hamburguer.classList.remove("fa-xmark");
+  }
 });
 
 for (let index = 1; index <= 4; index++) {
-    let element = document.getElementById("itemList"+index).addEventListener("click",function() {
-        lista.classList.remove("active")
+  let element = document
+    .getElementById("itemList" + index)
+    .addEventListener("click", function () {
+      lista.classList.remove("active");
+      hamburguer.classList.remove("fa-xmark");
     });
 }
